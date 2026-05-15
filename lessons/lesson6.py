9f6a941f0c290d20c4c339e72821f6464079d050
horeolis <bshmbaev@gmail.com>
2026-05-15 20:44:10 +0600

    added: lesson 6

--- lessons/lesson6.py
+++ lessons/lesson6.py
@@ -1,1 +1,13 @@
+nums = [2,7,11,15]
+target = 9
+
+for i in range(len(nums)-1):
+    print("проход номер", i)
+    if nums[i] + nums[i+1] == target:
+        print("[", i, ", ", i+1, "]")
+        break
+    else:
+        print("No match")
+
+
 
